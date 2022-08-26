8f83709fb957de7110b9d3091407da9374400d97
Pushkarniy Andrey <namedotnet@gmail.com>
2022-08-26 17:15:45 +0300

    Add rating stats. Add story

---
+++ src/stories/RatingStats.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { RatingStats } from "../components/ratingstats/RatingStats";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/RatingStats",
+  component: RatingStats,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof RatingStats>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof RatingStats> = (args) => (
+  <RatingStats {...args} />
+);
+
+export const RatingItem = Template.bind({});
+RatingItem.args = {
+  percent: 90,
+  rating: 4.5,
+  size: "sm",
+};
