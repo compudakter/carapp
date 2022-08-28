a0682de0c16abc2874a2ee2b66bf4466321ce229
compudakter <zloycompudakter@gmail.com>
2022-08-28 21:24:42 +0300

    button

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
-import styles from "./Button.module.scss";
+import  "./Button.scss";
+
+export type ButtonColor = 'red' 
+export type ButtonSize = 'md' | 'sm' 
+
 interface ButtonProps {
   onClick?: (e: any) => void;
+  color?:ButtonColor
+  size?:ButtonSize
   children?: any;
 }
-export const Button: FC<ButtonProps> = ({ onClick, children }) => {
+export const Button: FC<ButtonProps> = ({ onClick,color,size, children }) => {
+  const colorStyle = color ? `button_${color}`:''
+  const sizeType = size ? `button_${size}` : ''
   return (
     <button
-      className={`${styles.button} ${styles.button_red}`}
+      className={`button ${colorStyle} ${sizeType}`}
       onClick={onClick}
     >
       {children}
